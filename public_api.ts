64d9fdc2ac293a60967fd7d142aea9aeeeffd3d3
somratexel <somratexelgm@gmail.com>
2017-11-29 11:50:30 +0600

    publish v0.0.3

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,3 @@
-export * from './src/app/slider/slider.module'
+export * from './src/app/slider/slider.module';
+export { Slider } from './src/app/slider/class/slider';
+export { SliderComponent } from './src/app/slider/slider.component';
